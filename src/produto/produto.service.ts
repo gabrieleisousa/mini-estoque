cd34df37349c1d4373c397e3a95ccb76c1c72949
Gabriele Iris <gabriele.i.sousa@aluno.senai.br>
2026-08-20 15:09:12 -0300

    Atualização do CRUD

--- src/produto/produto.service.ts
+++ src/produto/produto.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateProdutoDto } from './dto/create-produto.dto';
 import { UpdateProdutoDto } from './dto/update-produto.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -7,7 +7,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class ProdutoService {
   constructor(private prisma: PrismaService) { }
 
-  criar(dados: CreateProdutoDto) {
+  async criar(dados: CreateProdutoDto) {
+    const produtoExistente = await this.prisma.produto.findFirst({
+      where: { nome: dados.nome }
+    })
+
+    if (produtoExistente) {
+      throw new ConflictException("Já existe um produto com este nome")
+    }
+  
+
     return this.prisma.produto.create({
       data: dados
     });
@@ -17,22 +26,44 @@ export class ProdutoService {
     return this.prisma.produto.findMany();
   }
 
-  buscarPorID(id: number) {
-    return this.prisma.produto.findUnique({
-      where: { id }
-    });
+  async buscarPorID(id: number) {
+    const produto = await this.prisma.produto.findUnique({
+    where: { id }
+   });
+
+   if (!produto) {
+    throw new ConflictException('Produto com o ID ${id} não foi encontrado')
+   }
+   return produto;
   }
 
-  atualizar(id: number, dados: UpdateProdutoDto) {
+  async atualizar(id: number, dados: UpdateProdutoDto) {
+    await this.buscarPorID(id);
+
+    if (dados.nome) {
+      const produtoExistente = await this.prisma.produto.findFirst({
+        where: {
+          nome: dados.nome,
+          NOT: { id }
+        }
+      });
+
+      if (produtoExistente)
+        throw new ConflictException("Já existe outro produto cadastrado com esse nome")
+    }
+
     return this.prisma.produto.update({
       where: { id },
       data: dados
     });
   }
 
-  deletar(id: number) {
+  async deletar(id: number) {
+
+    await this.buscarPorID(id);
+ 
     return this.prisma.produto.delete({
-      where: { id}
+      where: { id }
     });
   }
-}
+  }
